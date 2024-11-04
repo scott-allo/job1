be6c06d03aafac65db6531f9c6cc9203349e7689
scott <scott.allo@laplateforme.io>
2024-11-04 16:43:40 +0100

    save1

---
+++ Projet2.py
@@ -0,0 +1,41 @@
+"""
+N=0
+while N < 13:
+    print(N)
+    N += 1
+"""
+"""
+N=int(input("Saisir un entier N : "))
+Y = 1
+while Y <= N :
+    result = Y * 7
+    print(f"{Y} x {7} = {result} ")
+    Y += 1
+"""
+"""
+Tours=1
+while Tours <13:
+    r = Tours * 3 - 2
+    print(f"Tours {Tours}: {r}")
+    Tours += 1
+"""
+"""
+Tours=1
+Calcul=0
+while Tours <13:
+    r = Calcul + 2
+    print(f"Tours {Tours}: {r}")
+    Tours += 1
+    Calcul += 2
+"""
+"""
+pair=0
+impair=1
+while impair < 29 or pair < 29 :
+    pair += 2
+    impair += 2
+    print(f"pair:{pair} impair:{impair}")
+
+"""
+
+ 
